e21aeb2ca359e0c02c0c1c40f60bf1643c1a7e31
pr0digy91 <garunya91@gmail.com>
2024-09-30 00:08:36 +0000

    venv activation in entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,6 +6,9 @@ git config --global user.name "${GITHUB_ACTOR}"
 git config --global user.email "${INPUT_EMAIL}"
 git config --global --add safe.directory /github/workspace
 
+# Activate the virtual environment
+source /venv/bin/activate
+
 python3 /usr/bin/feed.py
 
 git add -A && git commit -m "Update Feed"
